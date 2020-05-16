1b8137ef6ce700ce00294f6eb440708200550b1c
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-05-16 19:21:45 +0530

    Everything modified working fine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, Linking} from 'react-native';
+import PlayScreen from './PlayScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  const [playMode, setPlayMode] = useState(false);
+  return playMode ? (
+    <PlayScreen />
+  ) : (
+    <View style={styles.container}>
+      <Text style={styles.header}>Rock Paper Scissors</Text>
+      <Text style={styles.emoji}>✊ ✋ ✌️</Text>
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={() => {
+            setPlayMode(true);
+          }}
+          title="Play"
+        />
+      </View>
+      <Text
+        style={styles.srcCode}
+        onPress={() =>
+          Linking.openURL(
+            'https://github.com/ShivamJoker/RN-Rock-Paper-Scissors',
+          )
+        }>
+        Source Code
+      </Text>
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 28,
+    margin: 10,
+  },
+  emoji: {
+    fontSize: 60,
+  },
+  btnContainer: {
+    marginTop: 60,
+    width: 240,
+  },
+  srcCode: {
+    position: 'absolute',
+    bottom: 20,
+    color: '#0069c0',
+    textDecorationLine: 'underline',
+  },
+});
